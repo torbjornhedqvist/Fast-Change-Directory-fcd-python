5abab77959b439ff50682dd19a72af375332bec3
Torbjorn Hedqvist <torbjorn.hedqvist@ericsson.com>
2022-08-07 10:25:21 +0200

    Initial commit

---
+++ fcd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (C) Torbjorn Hedqvist - All Rights Reserved
+# You may use, distribute and modify this code under the
+# terms of the MIT license. See LICENSE file in the project
+# root for full license information.
+#
+# This script works in conjunction with fcd.py
+# If fcd.py creates the files below it will
+#  a) change directory to the content of ~/.fcd_dir and
+#  b) execute the commands in ~/.fcd_cmd
+#
+fcd.py $@
+
+# Created on succesful call to fcd.py
+if [ -e ~/.fcd_dir ]; then
+    cd `cat ~/.fcd_dir`
+fi
+
+# Created if previous call to fcd.py had an attached extra command line
+if [ -e ~/.fcd_cmd ]; then
+    source ~/.fcd_cmd
+fi
