04128748c7bbedc0cd7b3b1089dc711784a1d4bf
Torbjorn Hedqvist <torbjorn.hedqvist@ericsson.com>
2023-07-04 23:55:03 +0200

    More refactoring towards object orientation and modularity.

--- fcd.py
+++ fcd.py
@@ -8,7 +8,7 @@ root for full license information.
 (F)ast (C)hange (D)irectory, fcd is a utility program to make it easier to create shortcuts
 to frequently visited directories. Since it's impossible to change your shell's current working
 directory from within the program this program requires some additional shell scripts,
-(included in the repository). See the README.md for more information.
+(included in the database). See the README.md for more information.
 """
 import sys
 import os
@@ -17,6 +17,22 @@ import json
 import readline
 
 VERSION = '1.1.0'
+
+class Color: # pylint: disable=too-few-public-methods
+    """ANSI Colors to be used in terminal output"""
+    BLUE = '\033[34m'
+    LIGHT_BLUE = '\033[94m'
+    RED = '\033[31m'
+    LIGHT_RED = '\033[91m'
+    CYAN = '\033[96m'
+    PURPLE = '\033[95m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[33m'
+    LIGHT_YELLOW = '\033[93m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+
 
 def parse_args() -> dict:
     """Parse the command line arguments. If any of the rules defined by this function
@@ -44,18 +60,52 @@ def parse_args() -> dict:
 class Files: # pylint: disable=too-few-public-methods
     """A class with all predefined filenames as class global attributes used by the program."""
 
-    REPOSITORY = f'{os.path.expanduser("~")}{"/.fcd.json"}'
-    DIR = f'{os.path.expanduser("~")}{"/.fcd_dir"}'
-    CMD = f'{os.path.expanduser("~")}{"/.fcd_cmd"}'
+    def __init__(self) -> None:
+        self._db_filename = f'{os.path.expanduser("~")}{"/.fcd.json"}'
+        self._dir_filename = f'{os.path.expanduser("~")}{"/.fcd_dir"}'
+        self._cmd_filename = f'{os.path.expanduser("~")}{"/.fcd_cmd"}'
+
+    # All getter methods
+    @property
+    def db_filename(self) -> str:
+        """Filename where the database will be stored"""
+        return self._db_filename
+
+    @property
+    def dir_filename(self) -> str:
+        """Filename where alias matching directory path will be stored"""
+        return self._dir_filename
+
+    @property
+    def cmd_filename(self) -> str:
+        """Filename where alias matching command will be stored"""
+        return self._cmd_filename
+
+    # All setter methods
+    @db_filename.setter
+    def db_filename(self, value: str) -> None:
+        """Set the database filename"""
+        self._db_filename = value
+
+    @dir_filename.setter
+    def dir_filename(self, value: str) -> None:
+        """Set the directory path filename"""
+        self._dir_filename = value
+
+    @cmd_filename.setter
+    def cmd_filename(self, value: str) -> None:
+        """Set the command filename"""
+        self._cmd_filename = value
+
 
 class TabComplete: # pylint: disable=too-few-public-methods
     """A TAB completer class for readline
 
-    Args: aliases is a list of strings pulled from the alias keyword in the repository and
+    Args: aliases is a list of strings pulled from the alias keyword in the database and
         will be used for comparison in the complete function provided to readline.
     """
 
-    def __init__(self, aliases: list):
+    def __init__(self, aliases: list) -> None:
         self._aliases = aliases
 
     def complete(self, text: str, state: int) -> str:
@@ -64,111 +114,93 @@ class TabComplete: # pylint: disable=too-few-public-methods
         return results[state]
 
 
-class Color: # pylint: disable=too-few-public-methods
-    """ANSI Colors to be used in terminal output"""
-    BLUE = '\033[34m'
-    LIGHT_BLUE = '\033[94m'
-    RED = '\033[31m'
-    LIGHT_RED = '\033[91m'
-    CYAN = '\033[96m'
-    PURPLE = '\033[95m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[33m'
-    LIGHT_YELLOW = '\033[93m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    RESET = '\033[0m'
+class Db:
+    """Class to load and save the database file"""
 
-
-class Repository:
-    """Class to load and save the repository"""
-    def __init__(self) -> None:
-        """ Constructor
-        """
+    def __init__(self, db_file: str) -> None:
+        self._db_file = db_file
 
     def load(self) -> dict:
-        """Load the repository from file
+        """Load the database from file
 
-        Returns: repository. The whole repository as a dict of dicts with records where each
+        Returns: database. The whole database as a dict of dicts with records where each
         record is a dict containing the key value pairs of directory and command.
         """
         try:
-            if os.path.exists(Files.REPOSITORY):
-                with open(Files.REPOSITORY, encoding = 'utf-8') as file:
-                    repository = json.load(file)
+            if os.path.exists(self._db_file):
+                with open(self._db_file, encoding = 'utf-8') as file:
+                    database = json.load(file)
             else:
-                print(f'{Color.LIGHT_YELLOW}No repository {Files.REPOSITORY} exists ', end='')
+                print(f'{Color.LIGHT_YELLOW}No database {self._db_file} exists ', end='')
                 print('in your home directory, first time usage?')
-                print('Creates a new repository with a dummy record which can be removed later.')
+                print('Creates a new database with a dummy record which can be removed later.')
                 print(Color.RESET, end='')
                 json_string = '{"dummy": {"directory": "/dummy", "command": ""}}'
-                repository = json.loads(json_string)
-                self.save(repository)
-            return repository
+                database = json.loads(json_string)
+                self.save(database)
+            return database
         except IOError as io_error:
             sys.exit(io_error)
         except json.decoder.JSONDecodeError as json_decoder_error:
-            print(f'{Files.REPOSITORY} is empty, please delete it and it will be created properly')
+            print(f'{self._db_file} is empty, please delete the file and execute again')
             sys.exit(json_decoder_error)
 
 
-    def save(self, repository: dict) -> None:
-        """Save the current repository in memory to file
+    def save(self, database: dict) -> None:
+        """Save the current database in memory to file
         """
         try:
-            with open(Files.REPOSITORY, 'w', encoding = 'utf-8') as file:
-                json.dump(repository, file)
+            with open(self._db_file, 'w', encoding = 'utf-8') as file:
+                json.dump(database, file)
         except IOError as io_error:
             sys.exit(io_error)
 
 
 class Fcd:
     """Main Class for (F)ast (C)hange (D)irectory"""
+
     def __init__(self) -> None:
-        """ Constructor
-        """
-        self._repository_handler = Repository()
-        self._repository = self._repository_handler.load()
+        self._files = Files()
+        self._db_handler = Db(self._files.db_filename)
+        self._db = self._db_handler.load()
 
         # Aliases to be used for tab completion
-        self._aliases = sorted(self._repository.keys(), key=str.lower)
+        self._aliases = sorted(self._db.keys(), key=str.lower)
         # A TabComplete instance prepared with a list of aliases created from all records in the
-        # repository.
+        # database.
         self._completer = TabComplete(self._aliases)
 
-
     def save_for_later_execution(self, alias: str):
-        """Save the directory path and command, (if not empty) to the separate files Files.DIR
-        and Files.CMD. These files will be used later by the external bash script to change
-        directory and execute the command if available.
+        """Save the directory path and command, (if not empty) to the separate files
+        self._files.dir_filename and self._files.cmd_filename. These files will be used later
+        by the external bash script to change directory and execute the command if available.
 
         Args:
             alias: alias as a string which is the key to the record containing the directory
             and commands to be saved.
         """
-        if os.path.isdir(self._repository[alias]['directory']):
-            # The directory path stored in the repository actually exists in the file system,
+        if os.path.isdir(self._db[alias]['directory']):
+            # The directory path stored in the database actually exists in the file system,
             # save the directory path to file.
-            dir_path = f'{self._repository[alias]["directory"]}\n'
+            dir_path = f'{self._db[alias]["directory"]}\n'
             try:
-                with open(Files.DIR, 'w', encoding = 'utf-8') as file:
+                with open(self._files.dir_filename, 'w', encoding = 'utf-8') as file:
                     file.write(dir_path)
             except IOError as io_error:
                 sys.exit(io_error)
 
-            if self._repository[alias]['command'] != '':
+            if self._db[alias]['command'] != '':
                 # This record have an associated command, save the cmd to file.
-                cmd = f'{self._repository[alias]["command"]}\n'
+                cmd = f'{self._db[alias]["command"]}\n'
                 try:
-                    with open(Files.CMD, 'w', encoding = 'utf-8') as file:
+                    with open(self._files.cmd_filename, 'w', encoding = 'utf-8') as file:
                         file.write(cmd)
                 except IOError as io_error:
                     sys.exit(io_error)
         else:
-            print(f'{Color.LIGHT_RED}Directory "{self._repository[alias]["directory"]}" ', end='')
+            print(f'{Color.LIGHT_RED}Directory "{self._db[alias]["directory"]}" ', end='')
             print(f'doesn\'t exist, recommended to remove record{Color.RESET}')
             sys.exit(1)
-
 
     def list_records(self, records: list, alias: str = '', show_cmd: bool = False,
                     use_colors: bool = True) -> None:
@@ -205,7 +237,6 @@ class Fcd:
                     print(f'{current_color}[{record[0]}] {record[1]}')
         print(Color.RESET, end='')
 
-
     def read_input(self, hook_message: str, prompt: str = 'fcd> ') -> str:
         """Common function to use the tab completer function and read an input line
 
@@ -237,25 +268,24 @@ class Fcd:
         """
         if len(sys.argv) == 1:
             # No arguments at all, including alias given from command line
-            # Indicates that the user would like to see the full content of the repository and
+            # Indicates that the user would like to see the full content of the database and
             # choose an alias from the listed records.
             alias = '' # tab pre_input_hook empty
         else:
             alias = args.get('alias')
 
-        if alias in self._repository:
-            # A complete alias found in repository
+        if alias in self._db:
+            # A complete alias found in database
             self.save_for_later_execution(alias)
         else:
             # Incomplete/partial alias, let's use tab completion
             self.list_records(records, alias)
-            while alias not in self._repository:
+            while alias not in self._db:
                 alias = self.read_input(alias)
-                if alias not in self._repository:
+                if alias not in self._db:
                     self.list_records(records, alias)
-            # Now we have a complete alias found in repository
+            # Now we have a complete alias found in database
             self.save_for_later_execution(alias)
-
 
     def add_handler(self, args: dict) -> None:
         """Handles the add command line argument and add a new record with alias and current
@@ -267,15 +297,14 @@ class Fcd:
         Returns: None
         """
         arg_add = args.get('add')
-        if arg_add not in self._repository:
+        if arg_add not in self._db:
             add_record = {"directory": os.getcwd(), "command": ''}
             print(f'Creating record [{arg_add}] "{os.getcwd()}"')
-            self._repository.update({arg_add : add_record})
-            self._repository_handler.save(self._repository)
+            self._db.update({arg_add : add_record})
+            self._db_handler.save(self._db)
         else:
             print(f'{Color.LIGHT_RED}Alias "{arg_add}" already exists, aborting!{Color.RESET}')
             sys.exit(1)
-
 
     def delete_handler(self, args: dict, records: list) -> None:
         """Handles the delete command line argument
@@ -287,19 +316,19 @@ class Fcd:
         Returns: None
         """
         arg_delete = args.get('delete')
-        if arg_delete is True or arg_delete not in self._repository:
+        if arg_delete is True or arg_delete not in self._db:
             # No or incomplete argument provided, let's go into interactive mode
             if arg_delete is True:
                 alias = '' # tab pre_input_hook empty
             else:
                 alias = arg_delete
             self.list_records(records, alias)
-            while alias not in self._repository:
+            while alias not in self._db:
                 alias = self.read_input(alias, 'fcd (delete entry)> ')
-                if alias not in self._repository:
+                if alias not in self._db:
                     self.list_records(records, alias)
             # Match found through tab completion
-            self._repository.pop(alias)
+            self._db.pop(alias)
             tmp_records = records
             for record in records:
                 if record[0] == alias:
@@ -308,15 +337,14 @@ class Fcd:
             print(f'"{alias}" deleted')
         else:
             # Exact argument match found
-            self._repository.pop(arg_delete)
+            self._db.pop(arg_delete)
             tmp_records = records
             for record in records:
                 if record[0] == arg_delete:
                     tmp_records.remove(record)
             records = tmp_records
             print(f'"{arg_delete}" deleted')
-        self._repository_handler.save(self._repository)
-
+        self._db_handler.save(self._db)
 
     def command_handler(self, args: dict, records: list) -> None:
         """Handles the command line argument for adding or updating a 'command' to a record.
@@ -327,15 +355,15 @@ class Fcd:
 
         Returns: None
         """
-        if args.get('add') in self._repository:
+        if args.get('add') in self._db:
             alias = args.get('add')
         else:
             alias = '' # tab pre_input_hook empty
             self.list_records(records, alias, True)
             print("Select which entry to add or update command:")
-            while alias not in self._repository:
+            while alias not in self._db:
                 alias = self.read_input(alias, 'fcd (select entry)> ')
-                if alias not in self._repository:
+                if alias not in self._db:
                     self.list_records(records, alias, True)
 
         if args.get('command') is True:
@@ -344,34 +372,33 @@ class Fcd:
         else:
             cmd = args.get('command')
 
-        self._repository[alias]['command'] = cmd
+        self._db[alias]['command'] = cmd
         print(f'Updated or added command to record: [{alias}] ', end='')
-        print(f'{self._repository[alias]["directory"]} : {self._repository[alias]["command"]}')
-        self._repository_handler.save(self._repository)
-
+        print(f'{self._db[alias]["directory"]} : {self._db[alias]["command"]}')
+        self._db_handler.save(self._db)
 
     def clean_up(self) -> None:
-        """ Cleanup from previous execution
+        """ Cleanup and remove files from previous execution
         """
         try:
-            if os.path.exists(Files.DIR):
-                os.remove(Files.DIR)
-            if os.path.exists(Files.CMD):
-                os.remove(Files.CMD)
+            if os.path.exists(self._files.dir_filename):
+                os.remove(self._files.dir_filename)
+            if os.path.exists(self._files.cmd_filename):
+                os.remove(self._files.cmd_filename)
         except OSError as io_error:
             sys.exit(io_error)
 
     def execute(self, args) -> None:
         """ Main Fcd execution
         """
-        self.clean_up() # Remove Files.DIR and Files.CMD if they exists.
+        self.clean_up()
 
         # Create a list of all records in alphabetic order which will be used as a helper in
         # combination with the tab completion.
         records = []
         for alias in self._aliases:
-            record = [alias, self._repository[alias]['directory'],
-                      self._repository[alias]['command']]
+            record = [alias, self._db[alias]['directory'],
+                      self._db[alias]['command']]
             records.append(record)
 
         try:
